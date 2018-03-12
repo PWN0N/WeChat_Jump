d572589adc461c80ac56c094db9ac4469afc00eb
PWN0N <juangshin@gmail.com>
2018-03-12 22:23:55 +0800

    Change toUse SURF,FLANN and Ransac To Get The Match Points

--- WeChat_Jump/WeChat_Jump.cpp
+++ WeChat_Jump/WeChat_Jump.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main()
 {
-	//Create SIFT class pointer
+	//Create SURF class pointer
 	Ptr<Feature2D> f2d = xfeatures2d::SURF::create();
 	Mat img_1 = imread("flag.png");
 	Mat img_2 = imread("5.png");
@@ -23,13 +23,116 @@ int main()
 	f2d->compute(img_1, keypoints_1, descriptors_1);
 	f2d->compute(img_2, keypoints_2, descriptors_2);
 	//Matching descriptor vector using BFMatcher
-	BFMatcher matcher;
+	FlannBasedMatcher matcher;
 	vector<DMatch> matches;
 	matcher.match(descriptors_1, descriptors_2, matches);
+	double max_dist = 0; double min_dist = 100;
+	//-- Quick calculation of max and min distances between keypoints  
+	for (int i = 0; i < descriptors_1.rows; i++)
+	{
+		double dist = matches[i].distance;
+		if (dist < min_dist) min_dist = dist;
+		if (dist > max_dist) max_dist = dist;
+	}
+	cout << "-- Max dist :" << max_dist << endl;
+	cout << "-- Min dist :" << min_dist << endl;
+
+	//-- Draw only "good" matches (i.e. whose distance is less than 0.6*max_dist )  
+	//-- PS.- radiusMatch can also be used here.  
+	vector< DMatch > good_matches;
+	for (int i = 0; i < descriptors_1.rows; i++)
+	{
+		if (matches[i].distance < 0.6*max_dist)
+		{
+			good_matches.push_back(matches[i]);
+		}
+	}
+
+
+	//m_LeftKey、m_RightKey、m_Matches已经被计算出来，分别是提取的关键点及其匹配，下面直接计算F
+
+	// 分配空间
+	int ptCount = (int)matches.size();
+	Mat p1(ptCount, 2, CV_32F);
+	Mat p2(ptCount, 2, CV_32F);
+
+	// 把Keypoint转换为Mat
+	Point2f pt;
+	for (int i = 0; i<ptCount; i++)
+	{
+		pt = keypoints_1[matches[i].queryIdx].pt;
+		p1.at<float>(i, 0) = pt.x;
+		p1.at<float>(i, 1) = pt.y;
+
+		pt = keypoints_2[matches[i].trainIdx].pt;
+		p2.at<float>(i, 0) = pt.x;
+		p2.at<float>(i, 1) = pt.y;
+	}
+
+
+	// RANSAC
+	Mat m_Fundamental;
+	vector<uchar> m_RANSACStatus;
+
+	m_Fundamental = findFundamentalMat(p1, p2, m_RANSACStatus, FM_RANSAC);
+
+	int OutlinerCount = 0;
+	for (int i = 0; i<ptCount; i++)
+	{
+		if (m_RANSACStatus[i] == 0) // 野点
+		{
+			OutlinerCount++;
+		}
+	}
+
+	// 计算内点
+	vector<Point2f> m_LeftInlier;
+	vector<Point2f> m_RightInlier;
+	vector<DMatch> m_InlierMatches;
+	// 上面三个变量用于保存内点和匹配关系
+	int InlinerCount = ptCount - OutlinerCount;
+	m_InlierMatches.resize(InlinerCount);
+	m_LeftInlier.resize(InlinerCount);
+	m_RightInlier.resize(InlinerCount);
+	InlinerCount = 0;
+	for (int i = 0; i<ptCount; i++)
+	{
+		if (m_RANSACStatus[i] != 0)
+		{
+			m_LeftInlier[InlinerCount].x = p1.at<float>(i, 0);
+			m_LeftInlier[InlinerCount].y = p1.at<float>(i, 1);
+			m_RightInlier[InlinerCount].x = p2.at<float>(i, 0);
+			m_RightInlier[InlinerCount].y = p2.at<float>(i, 1);
+			m_InlierMatches[InlinerCount].queryIdx = InlinerCount;
+			m_InlierMatches[InlinerCount].trainIdx = InlinerCount;
+			InlinerCount++;
+		}
+	}
+
+	// 把内点转换为drawMatches可以使用的格式
+	vector<KeyPoint> key1(InlinerCount);
+	vector<KeyPoint> key2(InlinerCount);
+	KeyPoint::convert(m_LeftInlier, key1);
+	KeyPoint::convert(m_RightInlier, key2);
+
+	// 显示计算F过后的内点匹配
+	//Mat m_matLeftImage;
+	//Mat m_matRightImage;
+	// 以上两个变量保存的是左右两幅图像
+	Mat OutImage;
+	drawMatches(img_1, key1, img_2, key2, m_InlierMatches, OutImage);
+
+	//stereoRectifyUncalibrated();
+
 	Mat img_matches;
-	drawMatches(img_1, keypoints_1, img_2, keypoints_2, matches, img_matches);
-	namedWindow("result", WINDOW_NORMAL);
-	imshow("result", img_matches);
+	drawMatches(img_1, keypoints_1, img_2, keypoints_2,
+		good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+		vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+	namedWindow("Match", WINDOW_NORMAL);
+	imshow("Match", img_matches);
+
+	namedWindow("Match2", WINDOW_NORMAL);
+	imshow("Match2", OutImage);
 	waitKey(0);
-	return 0;
 }
